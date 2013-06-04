fb15948f10785f9bf7d2db0846e380e0b73a8d3d
Joe Shepherd <joeshepmedia@gmail.com>
2013-06-04 10:33:07 -0500

    Add browse list of posts

--- test/integration/user_posts_article_test.rb
+++ test/integration/user_posts_article_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
-class UserPostsArticleTest < ActionDispatch::IntegrationTest
-
+class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
   test "view a list of articles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say")
     Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writer")
 
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
+    visit '/posts'
+    sleep 1
     #assume we have a page variable
-    assert_include page.content, "???"
+    assert_include page.body, "All Posts"
+    assert_include page.body, "First Post!"
+    assert_include page.body, "Wow, it's been a while!"
+
   end
   # test "the truth" do
   # assert true
